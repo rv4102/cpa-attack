96cbdf19c133207ef73f4865b755e8e9b2c9fcf2
Rushil Venkateswar <rushilv14@gmail.com>
2024-11-24 11:19:46 +0530

    added plots

--- src/cpa.py
+++ src/cpa.py
@@ -2,6 +2,7 @@ import sys
 import numpy as np
 import pandas as pd
 
+
 def main():
     if len(sys.argv) != 3:
         print("Usage: python cpa.py <traces_file> <hamm_file>")
@@ -11,7 +12,7 @@ def main():
     leakages_file = sys.argv[2]
 
     H = pd.read_csv(leakages_file, header=None).T
-    T = pd.read_csv(traces_file, header=None).T 
+    T = pd.read_csv(traces_file, header=None).T
 
     T_normalized = (T - T.mean()) / T.std()
     H_normalized = (H - H.mean()) / H.std()
@@ -19,6 +20,8 @@ def main():
     # Compute the correlation matrix
     correlation_matrix = np.dot(T_normalized.values, H_normalized.values.T) / T.shape[1]
     correlation_df = pd.DataFrame(correlation_matrix, index=T.index, columns=H.index)
+
+    correlation_df.to_csv("results/correlation.csv", header=False, index=False)
     max_index = correlation_df.stack().idxmax()
 
     guessed_key = max_index[1]
